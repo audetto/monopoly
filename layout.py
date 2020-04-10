b4d5df888e043ce367a8e0ed79d38da5d4c4687b
Andrea Odetti <mariofutire@gmail.com>
2020-04-10 11:14:56 +0100

    Make form more inline.

--- layout.py
+++ layout.py
@@ -28,44 +28,44 @@ def create_layout(human_players: List[str], bank: str, game: Dict):
     pay_receive_tab = dbc.Card(dbc.CardBody(dbc.Form([
         dbc.FormGroup([
             dbc.Label('Pay', html_for='pay-player'),
-            dbc.Select(id='pay-player', options=player_selects)
-        ]),
+            dbc.Col(dbc.Select(id='pay-player', options=player_selects))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Receive', html_for='receive-player'),
-            dbc.Select(id='receive-player', options=player_selects)
-        ]),
+            dbc.Col(dbc.Select(id='receive-player', options=player_selects))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Amount', html_for='amount-pay'),
-            dbc.Input(id='pay-amount', type='numeric')
-        ]),
+            dbc.Col(dbc.Input(id='pay-amount', type='numeric'))
+        ], row=True),
         dbc.Button('Pay', id='pay-button', color="danger", className="mr-1")
     ])))
 
     property_dealing_tab = dbc.Card(dbc.CardBody(dbc.Form([
         dbc.FormGroup([
             dbc.Label('Seller', html_for='trade-seller'),
-            dbc.Select(id='trade-seller', options=actual_players_selects)
-        ]),
+            dbc.Col(dbc.Select(id='trade-seller', options=actual_players_selects))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Buyer', html_for='trade-buyer'),
-            dbc.Select(id='trade-buyer', options=actual_players_selects)
-        ]),
+            dbc.Col(dbc.Select(id='trade-buyer', options=actual_players_selects))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Property', html_for='trade-property'),
-            dbc.Select(id='trade-property', options=EMPTY_SELECT)
-        ]),
+            dbc.Col(dbc.Select(id='trade-property', options=EMPTY_SELECT))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Price', html_for='trade-price'),
-            dbc.Input(id='trade-price', type='numeric')
-        ]),
+            dbc.Col(dbc.Input(id='trade-price', type='numeric'))
+        ], row=True),
         dbc.Button('Trade', id='trade-button', color="success", className="mr-1"),
     ])))
 
     extra_tab = dbc.Card(dbc.CardBody(dbc.Form([
         dbc.FormGroup([
             dbc.Label('Player', html_for='extra-player'),
-            dbc.Select(id='extra-player', options=human_players_selects)
-        ]),
+            dbc.Col(dbc.Select(id='extra-player', options=human_players_selects))
+        ], row=True),
         dbc.Button('Go', id='go-button', color="success", className="mr-1"),
         dbc.Button('Income Tax', id='income-tax-button', color="dark", className="mr-1"),
         dbc.Button('Super tax', id='super-tax-button', color="secondary", className="mr-1"),
@@ -75,12 +75,12 @@ def create_layout(human_players: List[str], bank: str, game: Dict):
     mortgage_tab = dbc.Card(dbc.CardBody(dbc.Form([
         dbc.FormGroup([
             dbc.Label('Player', html_for='mortgage-player'),
-            dbc.Select(id='mortgage-player', options=human_players_selects)
-        ]),
+            dbc.Col(dbc.Select(id='mortgage-player', options=human_players_selects))
+        ], row=True),
         dbc.FormGroup([
             dbc.Label('Property', html_for='mortgage-property'),
-            dbc.Select(id='mortgage-property', options=EMPTY_SELECT)
-        ]),
+            dbc.Col(dbc.Select(id='mortgage-property', options=EMPTY_SELECT))
+        ], row=True),
         dbc.Button('Mortgage', id='mortgage-button', color="danger", className="mr-1"),
         dbc.Button('Unmortgage', id='unmortgage-button', color="success", className="mr-1"),
     ])))
